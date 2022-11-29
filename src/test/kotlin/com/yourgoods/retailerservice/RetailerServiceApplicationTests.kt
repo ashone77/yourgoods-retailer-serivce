e3b7e1c4bd83980204ea8f16335305ecc2c974d1
Aswin Haridas <aswinvharidas@gmail.com>
2022-11-29 15:17:15 +0530

    added all files

---
+++ src/test/kotlin/com/yourgoods/retailerservice/RetailerServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.yourgoods.retailerservice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class RetailerServiceApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
